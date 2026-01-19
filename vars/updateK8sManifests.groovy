5aa7d0c55279e31f357e2e6fb4f0c75ce2d8b173
Omar Reda Beshir <omarreda011284@gmail.com>
2026-01-19 16:32:19 +0200

    Refactor updateK8sManifests function parameters

--- vars/updateK8sManifests.groovy
+++ vars/updateK8sManifests.groovy
@@ -1,19 +1,20 @@
-def call(String repo, String imageName, String tag, creds) {
+def call(String repoUrl, String imageName, String tag, String gitToken) {
 
     sh """
-    rm -rf manifests
-    git clone https://${creds_USR}:${creds_PSW}@${repo.replace("https://","")} manifests
+      rm -rf manifests
+      git clone https://${gitToken}@github.com/OMAR-BESHIR/k8s-manifests.git manifests
 
-    cd manifests
+      cd manifests
 
-    sed -i 's|image: .*|image: ${imageName}:${tag}|' deployment.yaml
+      sed -i 's|image: .*|image: ${imageName}:${tag}|' deployment.yaml
 
-    git config user.email "jenkins@ci.com"
-    git config user.name "jenkins"
+      git config user.email "jenkins@local"
+      git config user.name "jenkins"
 
-    git add deployment.yaml
-    git commit -m "Update image to ${imageName}:${tag}"
-    git push
+      git add .
+      git commit -m "Update image to ${imageName}:${tag}" || echo "No changes to commit"
+      git push origin main
     """
 }
+
 
